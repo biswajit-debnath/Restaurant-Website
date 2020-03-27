9bbef87c503e2c1a8906166bcfa18c5f28bce589
biswajit-debnath <biswajitdebnath405@gmail.com>
2020-03-28 03:15:32 +0530

    navbar and animation and lots

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 $(document).ready(function(){
 
+	//nav bar toggler btn
+	$('.navbar-toggler').click(function(){
+		$('.navbar-toggler').toggleClass('change')
+
+	})
+
+	//navbar padd less and making fixed after 718px scrol 	led
+	$(window).scroll(function(){
+		let position = $(this).scrollTop(); //scrolled position i.e number of pixels down 
+		if(position >= 718) {
+			$('.navbar').addClass('navbar_background');
+			$('.navbar').addClass('fixed-top')
+		}
+		else {
+			$('.navbar').removeClass('navbar_background');
+			$('.navbar').removeClass('fixed-top')	
+		}
+	})
+
+	//smooth scroll
+	$('.nav-item a').click(function(link){
+		link.preventDefault();
+	})
+
+
 	//water ripples
 	$("#header .container, .container_info").ripples({
   		dropRadius: 25,
